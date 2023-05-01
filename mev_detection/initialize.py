f2d55d1a615d2c818258d2b389ae32499fd2acb5
etelpmoc <etelmok@gmail.com>
2023-05-01 19:28:56 +0900

    Make custom_models folder in initialize phase

--- mev_detection/initialize.py
+++ mev_detection/initialize.py
@@ -1,9 +1,14 @@
+import os
+
 if __name__ == "__main__":
-    with open("settings.py", 'w') as f:
-        f.write("""DB_HOST=''\n""")
-        f.write("""DB_USER=''\n""")
-        f.write("""DB_PASSWORD=''\n""")
-        f.write("""DB_NAME='MEV'\n""")
-        f.write("""ERIGON_HOST=''\n""")
-        f.write("""ERIGON_PORT=8545\n""")
+    if "settings.py" not in os.listdir('.'):
+        with open("settings.py", 'w') as f:
+            f.write("""DB_HOST=''\n""")
+            f.write("""DB_USER=''\n""")
+            f.write("""DB_PASSWORD=''\n""")
+            f.write("""DB_NAME='MEV'\n""")
+            f.write("""ERIGON_HOST=''\n""")
+            f.write("""ERIGON_PORT=8545\n""")
     
+    if "custom_models" not in os.listdir('.'):
+        os.mkdir("custom_models")
